910aa43e5d51e484d5a55a5c58ffa5b978976af0
Bryan Miguel Flores Garcia <bryan.flores3210@alumnos.udg.mx>
2025-09-18 16:56:48 -0600

    Creacion Tareas

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+Route::get('tarea',function(){
+    return  view('lista-tareas');
+}
+);
+Route::get('tarea/crear',function(){
+    return view('crear-tarea');
+}
+);
+Route::post('tarea/crear',function(){
+    return 'TAREA CREADA';
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Volt::route('settings/profile', 'settings.profile')->name('profile.edit');
+    Volt::route('settings/password', 'settings.password')->name('password.edit');
+    Volt::route('settings/appearance', 'settings.appearance')->name('appearance.edit');
+});
+
+require __DIR__.'/auth.php';
