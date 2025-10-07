34c010580b54a4d4880700ca38041df9de608b2d
Bryan Miguel Flores Garcia <bryan.flores3210@alumnos.udg.mx>
2025-10-07 14:58:25 -0600

    Implementa creacion de tarea -crea formulario para creacion de atrea (create-tarea) -implementa meotodo store de tareaController para recibir datos, crear registro y redireccionar a listado de tareas.

--- resources/views/tareas/create-tarea.blade.php
+++ resources/views/tareas/create-tarea.blade.php
@@ -3,10 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tarea: Crear</title>
 </head>
 <body>
     <h1>Creacion Tarea</h1>
-    <form action="{{ route('tarea') }}"
+    <form action="{{ route('tarea.store') }}" method="post">
+        @csrf
+        <label for="titulo">Titulo:</label>
+        <input type="text" id="titulo" name="titulo" required>
+        <br>
+        <label for="descripcion">Descripcion:</label>
+        <textarea  id="descripcion" name="descripcion" required></textarea>
+        <br>
+        <button type="submit">Guardar</button>
+    </form>
 </body>
 </html>
