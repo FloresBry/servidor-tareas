7f6ed1ec61d0a9a020a95b14b2b33d3b13c064f8
Bryan Miguel Flores Garcia <bryan.flores3210@alumnos.udg.mx>
2025-10-21 23:12:39 -0600

    Implementa bootstrap en el archivo edit-alumno
    Agregamos algunos componentes relacionado al formulario y el boton de guarder las preguntas contestadas, ademas que dejamos en codigo las advertencias si los usuarios no ingresan ningun dato.

--- resources/views/alumnos/edit-alumno.blade.php
+++ resources/views/alumnos/edit-alumno.blade.php
@@ -1,35 +1,67 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tarea: Editar</title>
-</head>
-<body>
-    <h1>Editar Alumno</h1>
+@php
+    $titulo="Editar Alumno ".$alumno->nombre;
+    $rutas_Alumnos=[
+        ['ruta'=>'alumno.index','etiqueta'=>'Listado de Alumnos'],
+        ['ruta'=>'alumno.create','etiqueta'=>'Crear Alumno']
+    ];
+@endphp
+<x-html_estructura>
+<x-encabezado :name="$titulo" :rutas="$rutas_Alumnos"/>
+<x-tamanio_formulario>
+    <x-titulo>Editar Alumno</x-titulo>
     @include('form-error')
     <form action="{{ route('alumno.update',$alumno->id) }}" method="post">
         @csrf
         @method('PATCH')
-        <label for="codigo">Codigo:</label>
-        <input type="text" id="codigo" name="codigo" value="{{  old('codigo') ?? $alumno->codigo }}" >
+        <div class="col-md-12">
+        <label for="codigo" class="form-label">Codigo:</label>
+        <input  class="form-control" type="text" id="codigo" name="codigo" value="{{  old('codigo') ?? $alumno->codigo }}" >
         <br>
-        <label for="nombre">Nombre:</label>
-        <input type="text" id="nombre" name="nombre" value="{{  old('nombre') ?? $alumno->nombre }}" >
+        @error('codigo')
+            <div class="alert alert-danger">{{ $message }}</div>    
+        @enderror
+        </div>
+        <div class="col-md-12">
+        <label for="nombre" class="form-label">Nombre:</label>
+        <input class="form-control" type="text" id="nombre" name="nombre" value="{{  old('nombre') ?? $alumno->nombre }}" >
         <br>
-        <label for="correo">Correo:</label>
-        <input type="email" id="correo" name="correo" value="{{  old('correo') ?? $alumno->correo }}" >
+        @error('nombre')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+        </div>
+        <div class="col-md-12">
+        <label for="correo" class="form-label">Correo:</label>
+        <input class="form-control" type="email" id="correo" name="correo" value="{{  old('correo') ?? $alumno->correo }}" >
         <br>
-        <label for="fecha_nacimiento">Fecha de Nacimiento:</label>
-        <input type="date" id="fecha_nacimiento" name="fecha_nacimiento" value="{{  old('fecha_nacimiento') ?? $alumno->fecha_nacimiento }}" >
+        @error('correo')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+        </div>
+        <div class="col-md-12">
+        <label for="fecha_nacimiento" class="form-label">Fecha de Nacimiento:</label>
+        <input class="form-control"  type="date" id="fecha_nacimiento" name="fecha_nacimiento" value="{{  old('fecha_nacimiento') ?? $alumno->fecha_nacimiento }}" >
         <br>
-        <label for="sexo">Sexo:</label>
-        <input type="text" id="sexo" name="sexo" value="{{  old('sexo') ?? $alumno->sexo }}" >
+        @error('fecha_nacimiento')
+            <div class="alert alert-danger">{{ $message }}</div>    
+        @enderror
+        </div>
+        <div class="col-md-12">
+        <label for="sexo" class="form-label">Sexo:</label>
+        <input class="form-control" type="text" id="sexo" name="sexo" value="{{  old('sexo') ?? $alumno->sexo }}" >
         <br>
-        <label for="carrera">Carrera:</label>
-        <input type="text" id="carrera" name="carrera" value="{{  old('carrera') ?? $alumno->carrera }}" >
+        @error('sexo')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror 
+        </div>
+        <div class="col-md-12">  
+        <label for="carrera" class="form-label">Carrera:</label>
+        <input class="form-control" type="text" id="carrera" name="carrera" value="{{  old('carrera') ?? $alumno->carrera }}" >
         <br>
-        <button type="submit">Guardar</button>
+        @error('carrera')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+        </div>
+        <x-boton_guardar/>
     </form>
-</body>
-</html>
+</x-tamanio_formulario>
+</x-html_estructura>
